7d383564fdf1d8813ab85c28f7b8decb6c0821d6
DylanClarkLS <dylanclarklopes@gmail.com>
2023-10-16 12:11:38 -0300

    first commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,33 @@
+alert("Boas vindas ao jogo do número secreto");
+
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 1;
+
+// Equanto o chute não for igual ao número secreto
+while(chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // Testa se o chute é igual número secreto
+    if(chute == numeroSecreto){
+        break;    
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        // Incrementação
+        tentativas++;
+    }
+}
+
+// Operador Ternário
+let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+/* if(tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} tentativa`);
+} */
